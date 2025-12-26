d8f8a0dd1a5638bd9a8cbb879a7145c797af75f0
Andrei Shkolnyi <ashkolny@klerk.ru>
2025-12-26 17:10:30 +0300

    feat(main): add grpc, integrate docker, change go.yaml

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,48 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/AndreiShkolnyi/go-auth/pkg/auth_v1"
+	"github.com/brianvoe/gofakeit/v7"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+const grpcPort = 50051
+
+type server struct {
+	auth_v1.UnimplementedAuthV1Server
+}
+
+func (s *server) Get(ctx context.Context, req *auth_v1.GetRequest) (*auth_v1.GetResponse, error) {
+	log.Printf("Note id: #{req.GetId()}")
+
+	return &auth_v1.GetResponse{
+		Id:     gofakeit.Int64(),
+		Name:   gofakeit.Name(),
+		Email:  gofakeit.Email(),
+		Role:   auth_v1.Role_USER,
+			CreatedAt: timestamppb.New(gofakeit.Date()),
+			UpdatedAt: timestamppb.New(gofakeit.Date()),
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%v", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	auth_v1.RegisterAuthV1Server(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/AndreiShkolnyi/go-auth
 
 go 1.24.4
+
+require (
+	github.com/brianvoe/gofakeit/v7 v7.12.1
+	golang.org/x/net v0.48.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+	golang.org/x/text v0.32.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251222181119-0a764e51fe1b // indirect
+	google.golang.org/grpc v1.78.0 // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.6.0 // indirect
+	google.golang.org/protobuf v1.36.11 // indirect
+)
